8857962f84d4bd2d6216a13d3cbb6dde2cea7141
joyce wamocho <joywamo@gmail.com>
2024-09-18 15:00:04 +0300

    function-lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(hello){
+    return `${"HELLO"}`;
+}
+console.log(shout());
+
+function whisper(HELLO){
+    return `${"hello"}`;
+}
+console.log(whisper());
+
+function logShout(hello){
+    console.log(hello.toUpperCase());
+}
+
+function logWhisper(HELLO){
+    console.log(HELLO.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string.includes("Let's have dinner together!")) {  
+      return "I would love to!";
+    } else {
+      return "I'm not sure what you said.";  
+    }
+  }
+
+
+
+
+
+
+    
